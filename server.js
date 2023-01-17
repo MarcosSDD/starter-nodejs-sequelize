0ae9b590e719e9c8901eda73e6b570393481d109
Marcos Saldivia D <marcos.saldivia@gmail.com>
2023-01-17 14:02:40 -0300

    User table added in models and welcome message is created

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const portServer = 3000
 
 app.use(express.json())
 
+// Default message
+app.get('*', (request, response) => response.status(200).json({
+	message: "Wellcome to Rest Web Service",
+}));
+
 //Run Server
 app.listen(portServer, () => {
   console.log(`Server working in port ${portServer}`)
